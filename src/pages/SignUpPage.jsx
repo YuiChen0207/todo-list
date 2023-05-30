7503e9a56baf52121f3f6eabd97044c11bf4045b
YuiChen0207 <asd0935252359@gmail.com>
2023-05-30 19:50:42 +0800

    change to decodeToken to fix the webpack error

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,95 @@
+import {
+  AuthContainer,
+  AuthInputContainer,
+  AuthButton,
+  AuthLinkText,
+} from 'components/common/auth.styled';
+import { ACLogoIcon } from 'assets/images';
+import { AuthInput } from 'components';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
+import { useAuth } from 'contexts/AuthContext';
+
+const SignUpPage = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const { isAuthenticated, register } = useAuth;
+
+  const handleClick = async () => {
+    if (!username.length || !password.length || !email.length) return;
+
+    const success = await register({
+      username,
+      password,
+      email,
+    });
+    if (success) {
+      Swal.fire({
+        position: 'top',
+        title: '註冊成功！',
+        timer: 1000,
+        icon: 'success',
+        showConfirmButton: false,
+      });
+      return;
+    }
+    Swal.fire({
+      position: 'top',
+      title: '註冊失敗！',
+      timer: 1000,
+      icon: 'error',
+      showConfirmButton: false,
+    });
+  };
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/todos');
+    }
+  }, [navigate, isAuthenticated]);
+
+  return (
+    <AuthContainer>
+      <div>
+        <ACLogoIcon />
+      </div>
+      <h1>建立您的帳號</h1>
+
+      <AuthInputContainer>
+        <AuthInput
+          label={'帳號'}
+          value={username}
+          placeholder={'請輸入帳號'}
+          onChange={(nameInputValue) => setUsername(nameInputValue)}
+        />
+      </AuthInputContainer>
+
+      <AuthInputContainer>
+        <AuthInput
+          label={'Email'}
+          value={email}
+          placeholder={'請輸入 email'}
+          onChange={(emailInputValue) => setEmail(emailInputValue)}
+        />
+      </AuthInputContainer>
+
+      <AuthInputContainer>
+        <AuthInput
+          type="password"
+          label="密碼"
+          value={password}
+          placeholder="請輸入密碼"
+          onChange={(passwordInputValue) => setPassword(passwordInputValue)}
+        />
+      </AuthInputContainer>
+      <AuthButton onClick={handleClick}>註冊</AuthButton>
+      <Link to="/login">
+        <AuthLinkText>取消</AuthLinkText>
+      </Link>
+    </AuthContainer>
+  );
+};
+
+export default SignUpPage;
---
+++ src/pages/TodoPage.jsx
@@ -0,0 +1,175 @@
+import { createTodo, deleteTodo, getTodos, patchTodo } from 'api/todos';
+import { Footer, Header, TodoCollection, TodoInput } from 'components';
+import { useAuth } from 'contexts/AuthContext';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const TodoPage = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState([]);
+  const navigate = useNavigate();
+  const { isAuthenticated, currentMember } = useAuth;
+
+  const handleChange = (value) => {
+    setInputValue(value);
+  };
+
+  const handleAddTodo = async () => {
+    if (inputValue.length === 0) {
+      return;
+    }
+    try {
+      const data = await createTodo({
+        title: inputValue,
+        isDone: false,
+      });
+      setTodos((prevTodos) => {
+        return [
+          ...prevTodos,
+          {
+            id: data.id,
+            title: data.title,
+            isDone: data.isDone,
+            isEdit: false,
+          },
+        ];
+      });
+
+      setInputValue('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleKeyDown = async () => {
+    if (inputValue.length === 0) {
+      return;
+    }
+    try {
+      const data = await createTodo({
+        title: inputValue,
+        isDone: false,
+      });
+      setTodos((prevTodos) => {
+        return [
+          ...prevTodos,
+          {
+            id: data.id,
+            title: data.title,
+            isDone: data.isDone,
+            isEdit: false,
+          },
+        ];
+      });
+      setInputValue('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleToggleDone = async (id) => {
+    const currentTodo = todos.find((todo) => todo.id === id);
+    try {
+      await patchTodo({
+        id,
+        isDone: !currentTodo.isDone,
+      });
+      setTodos((prevTodos) => {
+        return prevTodos.map((todo) => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              isDone: !todo.isDone,
+            };
+          }
+          return todo;
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChangeMode = ({ id, isEdit }) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isEdit,
+          };
+        }
+        return { ...todo, isEdit: false };
+      });
+    });
+  };
+
+  const handleSave = async ({ id, title }) => {
+    try {
+      await patchTodo({
+        id,
+        title,
+      });
+      setTodos((prevTodos) => {
+        return prevTodos.map((todo) => {
+          if (todo.id === id) {
+            return { ...todo, title, isEdit: false };
+          }
+          return todo;
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await deleteTodo(id);
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const getTodosAsync = async () => {
+      try {
+        const todos = await getTodos();
+        setTodos(todos.map((todo) => ({ ...todo, isEdit: false })));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getTodosAsync();
+  }, []);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/login');
+    }
+  }, [navigate, isAuthenticated]);
+
+  return (
+    <div>
+      TodoPage
+      <Header userName={currentMember?.name} />
+      <TodoInput
+        inputValue={inputValue}
+        onChange={handleChange}
+        onAddTodo={handleAddTodo}
+        onKeyDown={handleKeyDown}
+      />
+      <TodoCollection
+        todos={todos}
+        onSave={handleSave}
+        onToggleDone={handleToggleDone}
+        onChangeMode={handleChangeMode}
+        onDelete={handleDelete}
+      />
+      <Footer todos={todos} />
+    </div>
+  );
+};
+
+export default TodoPage;
